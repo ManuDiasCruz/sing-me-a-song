7ee7b1ca486f954e8fc6569701d7082506b60036
Manuella Dias Cruz <manuella.dias@datarisk.io>
2022-08-01 16:16:52 -0300

    feat: add E2E tests for POST/recommendations

---
+++ front-end/cypress/e2e/recommendation.post.js
@@ -0,0 +1,69 @@
+import { faker } from "@faker-js/faker";
+
+import * as setup from "./setup.js";
+
+beforeEach(() => {
+    cy.resetPosts();
+});
+
+describe("E2E tests: POST /recommendations", () => {
+    it("Add a song", () => {
+        const {name, youtubeLink} = setup.createRecommendation();
+
+        cy.visit("http://localhost:3000/");
+        
+        cy.get("input").first().type(name);
+        cy.get("input").last().type(youtubeLink);
+
+        cy.intercept("POST", "/recommendations").as("createRecommendation");
+        cy.get("button").click();
+
+        cy.wait("@createRecommendation").then((res) => {
+            expect(res.response.statusCode).to.equals(201);
+        });      
+    });
+
+    it("Add a wrong link song", () => {
+        const {name, youtubeLink} = setup.createWrongLink();
+
+        cy.visit("http://localhost:3000/");
+        cy.get("input").first().type(name);
+        cy.get("input").last().type(youtubeLink);
+    
+        cy.intercept("POST", "/recommendations").as("createRecommendation");
+        cy.get("button").click();
+
+        cy.wait("@createRecommendation").then((res) => {
+            expect(res.response.statusCode).to.equals(422);
+        });
+    });
+
+    it("Add a empty infos song", () => {
+        cy.visit("http://localhost:3000/");
+    
+        cy.intercept("POST", "/recommendations").as("createRecommendation");
+        cy.get("button").click();
+
+        cy.wait("@createRecommendation").then((res) => {
+            expect(res.response.statusCode).to.equals(422);
+        });
+    });  
+    
+    it("should add more than 10 posts, only shows 10 posts", () => {
+        cy.visit("http://localhost:3000/");
+    
+        for (let i = 0; i < 15; i++) {
+            const {name, youtubeLink} = setup.createWrongLink();
+            cy.get("input").first().type(name);
+            cy.get("input").last().type(youtubeLink);
+        
+            cy.intercept("POST", "/recommendations").as("createRecommendation");
+            cy.get("button").click();
+            cy.wait("@createRecommendation");
+
+            cy.get('[data-identifier="vote-menu"]')
+                .should("have.length.gte", 1)
+                .and("have.length.lte", 10);
+        }
+    });
+});
